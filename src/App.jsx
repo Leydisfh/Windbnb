00eec399fd6fc3f4397f7d8afdb9e234e69849a3
Leydis <leydisfh@gmail.com>
2023-12-15 21:39:53 -0500

    Codigo refactorizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import { useState } from 'react';
-import {  IoLocationSharp  } from "react-icons/io5";
 import './App.css';
 import './Styles/Footer.css';
-import stays from '../stays.json'
 import Header from './Components/Header';
 import Box from './Components/Box';
-import SearchEdit from './Components/Search-edit';
+//import SearchEdit from './Components/Search-edit';
 
 
 function App() {
   const [location, setLocation] = useState('');
-  const [isLocation, setIslocation] = useState(false);
-  const [showList , setShowList] = useState(false); // Estado de la lista
 
-  const [locationOptions, setLocationOptions] = useState(location)
-  const [guestValues, setGuestValues] = useState('');
-  
+
+
   //Capturando el valor del location
   const changeLocation = (e) =>{
     e.preventDefault();
@@ -23,7 +18,7 @@ function App() {
   }
 
   //Controlando el search buuton inicial
-  const handleSearch = (e) =>{
+  /*const handleSearch = (e) =>{
     e.preventDefault()
     return  stays.find((item)=>{
       if(location === item.city || location === item.country){
@@ -31,53 +26,16 @@ function App() {
       }
       setShowList(true);
     })
-  }
-
-  // Funcion para controlar que lso valores no se repitan
-  const uniqueLocations = Array.from(
-    new Set(stays.map((item) => `${item.city}, ${item.country}`))
-  );
-
-// Creando elemento li del componente search
-  const locationList = location && showList &&(
-  uniqueLocations.map((item, index) => (
-      <li key={index}>
-        <IoLocationSharp style={{ fontSize: '20px' }} /> 
-        {item}
-      </li>
-
-  ))
- ) ;
-
-
- const onChangeValue = (e) =>{
-  e.preventDefault()
-  return setGuestValues(e.target.value)
- }
-
- const handleLocation =(e) =>{
-  return setLocationOptions(e.target.value);
- }
+  }*/
 
   return (
     <>
-      <Header 
+      <Header
         valueLocation={location}
         onChangeLocation = {changeLocation}
-        onSubmit ={handleSearch}
-       
+       // onSubmit ={handleSearch}
       />
-     { 
-      isLocation && (
-      <SearchEdit  
-      locationValue={locationOptions}
-      locationOptions ={locationList}
-      onChangeLocation= {handleLocation}
-      value = {guestValues}
-      onChangeGuest = {onChangeValue}
-      /> 
-      )
-      }   
+   
       <main>
 
      <Box />
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,42 @@
+import { useState } from "react";
+import stays from '../../stays.json'
 import { IoIosSearch } from "react-icons/io";
+import {  IoLocationSharp  } from "react-icons/io5";
 import logo from "../assets/img/logo.png";
 import "../Styles/Header.css";
 
 const Header = ({onSubmit, onChangeLocation, valueLocation}) => {
+ // Estado del Formulario
+const [isformActive, setIsFormActive] = useState(false)
+ 
+  // Manejando el formulario
+ const handleForm = () =>{
+  return setIsFormActive(true)
+ }
+
+  // Funcion para controlar que lso valores no se repitan
+  const uniqueLocations = Array.from(
+    new Set(stays.map((item) => `${item.city}, ${item.country}`))
+  );
+
+// Creando elemento li del componente search
+  const locationList = isformActive && (
+  uniqueLocations.map((item, index) => (
+      <li key={index}>
+        <IoLocationSharp style={{ fontSize: '20px' }} /> 
+        {item}
+      </li>
+
+  ))
+ ) ;
+
+
+
   return (
     <header className="bnb__header">
       <img src={logo} alt="windbnb logo" />
-      <nav className="bnb__header__nav">
-        <form className="bnb__header__nav__container" onSubmit={onSubmit}>
+      <nav className={ isformActive ? 'nav__form__active': "bnb__header__nav"}>
+        <form className= "bnb__header__nav__container" onSubmit={onSubmit} onClick={handleForm}>
           <input className="search__input "
             type="search"
             id="search"
@@ -21,6 +50,11 @@ const Header = ({onSubmit, onChangeLocation, valueLocation}) => {
               <IoIosSearch style={{fontSize:"20px"}}/> 
           </button>
         </form>
+        <div className="search__location">
+            <ul  className="search__location__options">
+                {locationList}
+            </ul>
+            </div>
       </nav>
     </header>
   );
